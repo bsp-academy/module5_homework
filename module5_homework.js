d4df62d4aa90ddfcac50c8138625318b7aef079e
bsp-academy <127314569+bsp-academy@users.noreply.github.com>
2023-06-02 13:24:58 +0800

    Add files via upload

---
+++ module5_homework.js
@@ -0,0 +1,12 @@
+let equip = new Map ([
+    ["X", "Y"]
+    ]);
+
+for (let elem of equip)
+    console.log(elem);
+
+for (let name of equip.keys())
+    console.log(name);
+
+for (let value of equip.values())
+    console.log(value);
